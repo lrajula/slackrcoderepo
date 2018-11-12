7b0e93b59a8eb00b636d6b676a1ae7ccfba2b77d
lrajula <sushanth.laxmi@gmail.com>
2018-11-12 17:37:26 +0530

    sample-pipeline create

---
+++ sample-pipeline.groovy
@@ -0,0 +1,28 @@
+pipeline {
+    agent { label 'master' }
+    stages {
+        stage('build') {
+            steps {
+                script{
+				echo "hello world"
+				currentBuild.result = 'SUCCESS'
+				env.STAGE = "SUCCESS"
+                }
+            }
+        }
+    }
+	post {
+     success {
+        notifyBuild("SUCCESS", env.STAGE)
+     }
+     failure {
+        notifyBuild('FAILED', env.STAGE)
+     }
+     aborted {
+        notifyBuild('ABORTED', env.STAGE)
+     }
+     unstable {
+        notifyBuild('UNSTABLE', env.STAGE)
+     }
+  }
+}
